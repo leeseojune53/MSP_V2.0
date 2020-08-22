64e4c8ecd6ec5f1b27dc1958dc140596408a1270
leeseojune <sung07288346@gmail.com>
2020-08-22 21:42:45 +0900

    Update 08.22.20 make wall, star and poop

---
+++ MSP/Assets/Script/Gamemanager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Gamemanager : MonoBehaviour
+{
+
+    public GameObject overset;
+
+    public Transform[] spawnPoint;
+    public GameObject Star;
+    public GameObject Poop;
+
+    Transform pos;
+
+    public void GameOver()
+    {
+        overset.SetActive(true);
+    }
+
+    public void GameRetry()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Makestar()
+    {
+        int Ranpoint = Random.Range(0, 128);
+
+        GameObject star = Instantiate(Star, spawnPoint[Ranpoint].position, spawnPoint[Ranpoint].rotation);
+        pos = spawnPoint[Ranpoint];
+        Rigidbody2D rigid = star.GetComponent<Rigidbody2D>();
+    }
+
+    public void Makepoop()
+    {
+        GameObject poop = Instantiate(Poop, pos.position, pos.rotation);
+        Makestar();
+    }
+}
--- MSP/Assets/Script/Player.cs
+++ MSP/Assets/Script/Player.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public Gamemanager manager;
 
     public int speed;
 
     Vector3 moveVelocity = Vector3.zero;
 
+    private void Awake()
+    {
+        manager.Makestar();
+    }
     void Update()
     {
+        
         Move();
     }
 
@@ -30,4 +36,19 @@ public class Player : MonoBehaviour
 
         transform.position += moveVelocity * speed * Time.deltaTime;
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Wall" )
+        {
+            speed = 0;
+            manager.GameOver();
+        }
+        if(collision.gameObject.tag == "Star")
+        {
+            Destroy(collision.gameObject);
+            manager.Makepoop();
+        }
+    }
+
 }
