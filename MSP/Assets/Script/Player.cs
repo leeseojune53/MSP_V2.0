c2b16ae64576eb4c260ba188079a245c0c85bb45
leeseojune <sung07288346@gmail.com>
2020-08-22 16:49:11 +0900

    first_update make player moving

---
+++ MSP/Assets/Script/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public int speed;
+
+    Vector3 moveVelocity = Vector3.zero;
+
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+            
+        if (Input.GetAxisRaw("Horizontal") > 0)
+            moveVelocity = Vector3.right;
+        else if (Input.GetAxisRaw("Horizontal") < 0)
+            moveVelocity = Vector3.left;
+        if (Input.GetAxisRaw("Vertical") > 0)
+            moveVelocity = Vector3.up;
+        else if (Input.GetAxisRaw("Vertical") < 0)
+            moveVelocity = Vector3.down;
+        
+
+
+        transform.position += moveVelocity * speed * Time.deltaTime;
+    }
+}
